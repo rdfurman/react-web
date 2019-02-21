7ada0bc71ac1b00e862352179a2b9914d047aa8c
rdfurman <randydfurman@gmail.com>
2019-02-21 13:11:56 -0700

    Fix public folder reference

--- src/routes/LudumDare.js
+++ src/routes/LudumDare.js
@@ -8,8 +8,8 @@ class DemonShanker2 extends Component {
     super(props);
 
     this.unityContent = new UnityContent(
-      "../demonshanker2/build/Builds.json",
-      "../demonshanker2/build/UnityLoader.js"
+      process.env.PUBLIC_URL + "/demonshanker2/build/Builds.json",
+      process.env.PUBLIC_URL + "/demonshanker2/build/UnityLoader.js"
     );
   }
 
